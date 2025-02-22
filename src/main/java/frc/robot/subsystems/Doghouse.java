206332813905f4c4e44fa43fb5b9c20f1d5e7dd2
Nick Avtges <navtges@gmail.com>
2025-02-22 18:03:08 -0500

    Doghouse refactor

--- src/main/java/frc/robot/subsystems/Doghouse.java
+++ src/main/java/frc/robot/subsystems/Doghouse.java
@@ -9,49 +9,38 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.CANrange;
 import com.ctre.phoenix6.hardware.TalonFX;
-import edu.wpi.first.math.util.Units;
 import com.ctre.phoenix6.signals.InvertedValue;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants;
 import frc.robot.util.preferenceconstants.DoublePreferenceConstant;
 
 public class Doghouse extends SubsystemBase {
   /** Creates a new Doghouse. */
-  private DoublePreferenceConstant p_funnelSlowSpeed =
-      new DoublePreferenceConstant("Doghouse/FunnelSlowSpeed", 0.2);
-
-  private DoublePreferenceConstant p_funnelFastSpeed =
-      new DoublePreferenceConstant("Doghouse/Funnel/FastSpeed", 1);
+  private DoublePreferenceConstant p_funnelSpeed =
+      new DoublePreferenceConstant("Doghouse/Funnel/Speed", 1);
   private DoublePreferenceConstant p_funnelCurrentLimit =
       new DoublePreferenceConstant("Doghouse/Funnel/CurrentLimit", 20);
   private DoublePreferenceConstant p_manipulatorInSpeed =
-      new DoublePreferenceConstant("Doghouse/Manipulator/InSpeed", 0.3);
-  private DoublePreferenceConstant p_manipulatorOutSpeed =
-      new DoublePreferenceConstant("Doghouse/Manipulator/OutSpeed", 0.3);
+      new DoublePreferenceConstant("Doghouse/Manipulator/InSpeed", -0.2);
+  private DoublePreferenceConstant p_manipulatorShootSpeed =
+      new DoublePreferenceConstant("Doghouse/Manipulator/ShootSpeed", -0.3);
   private DoublePreferenceConstant p_manipulatorCurrentLimit =
-      new DoublePreferenceConstant("Doghouse/Manipulator/CurrentLimit", 20);
-
-  private final DutyCycleOut m_funnelRequest = new DutyCycleOut(0.0);
+      new DoublePreferenceConstant("Doghouse/Manipulator/CurrentLimit", 120);
 
   private TalonFX m_funnel = new TalonFX(Constants.DOGHOUSE_FUNNEL_MOTOR, "rio");
   private TalonFX m_manipulator =
       new TalonFX(Constants.ELEVATOR_MANIPULATOR_MOTOR, Constants.RIO_CANBUS);
-
   private final CANrange m_doghousCANRange =
       new CANrange(Constants.DOGHOUSE_CANRANGE, Constants.RIO_CANBUS);
   private final CANrange m_coralRange =
       new CANrange(Constants.CORAL_CANRANGE, Constants.RIO_CANBUS);
 
-  private boolean hasCoral = false;
-  private boolean isIn = false;
-
-  public Trigger hasCoralTrigger = new Trigger(() -> hasCoral);
-  public Trigger isInTrigger = new Trigger(() -> isIn);
+  private final DutyCycleOut m_funnelRequest = new DutyCycleOut(0.0);
+  private final DutyCycleOut m_manipulatorRequest = new DutyCycleOut(0.0);
 
   public Doghouse() {
     // configure funnel
@@ -74,12 +63,10 @@ public class Doghouse extends SubsystemBase {
   private void configureCANrange() {
     CANrangeConfiguration coralRangecfg = new CANrangeConfiguration();
     CANrangeConfiguration doghouscfg = new CANrangeConfiguration();
-    // CANrangeConfiguration canRangerightcfg = new CANrangeConfiguration();
     coralRangecfg.FovParams.FOVRangeX = 6.75;
     doghouscfg.FovParams.FOVRangeX = 7.5;
     coralRangecfg.FovParams.FOVRangeY = 6.75;
     doghouscfg.FovParams.FOVRangeY = 27.0;
-    // canRangerightcfg.FovParams.FOVRangeY = 27.0;
 
     doghouscfg.ToFParams.UpdateFrequency = 50;
     coralRangecfg.ToFParams.UpdateFrequency = 50;
@@ -92,151 +79,108 @@ public class Doghouse extends SubsystemBase {
     doghouscfg.ProximityParams.MinSignalStrengthForValidMeasurement = 10000;
 
     m_doghousCANRange.getConfigurator().apply(doghouscfg);
-    // m_canRangeRight.getConfigurator().apply(canRangerightcfg);
     m_coralRange.getConfigurator().apply(coralRangecfg);
   }
 
-  public boolean isCoralDetected() {
-    return m_coralRange.getIsDetected().getValue();
-  }
-
-  public boolean isDoghouseDetected() {
-    return m_doghousCANRange.getIsDetected().getValue();
-  }
-
-  public Trigger hasCoral() {
-    return hasCoralTrigger;
-  }
-
-  public Trigger isIn() {
-    return isInTrigger;
-  }
-
-  private void setSpeed(double output) {
+  private void setFunnelSpeed(double output) {
     m_funnel.setControl(m_funnelRequest.withOutput(output));
   }
 
-  private void stopMoving() {
-    setSpeed(0.0);
+  private void setManipulatorSpeed(double output) {
+    m_manipulator.setControl(m_manipulatorRequest.withOutput(output));
   }
 
-  private void moveSlow() {
-    setSpeed(p_funnelSlowSpeed.getValue());
+  private boolean hasCoral() {
+    return m_coralRange.getIsDetected().getValue();
   }
 
-  private void moveFast() {
-    setSpeed(p_funnelFastSpeed.getValue());
+  private boolean isBlocked() {
+    return m_doghousCANRange.getIsDetected().getValue();
+  }
+
+  private void funnelStop() {
+    setFunnelSpeed(0.0);
+  }
+
+  private void funnelGo() {
+    setFunnelSpeed(p_funnelSpeed.getValue());
   }
 
   private void manipulatorStop() {
-    m_manipulator.setControl(new DutyCycleOut(0.0));
+    setManipulatorSpeed(0.0);
   }
 
   private void manipulatorIn() {
-    m_manipulator.setControl(new DutyCycleOut(p_manipulatorInSpeed.getValue()));
+    setManipulatorSpeed(p_manipulatorInSpeed.getValue());
   }
 
-  private void manipulatorOut() {
-    m_manipulator.setControl(new DutyCycleOut(p_manipulatorOutSpeed.getValue()));
-  }
-
-  private void manipulatorReverse() {
-    m_manipulator.setControl(new DutyCycleOut(0.1));
+  private void manipulatorShoot() {
+    setManipulatorSpeed(p_manipulatorShootSpeed.getValue());
   }
 
   private void manipulatorSlow() {
-    m_manipulator.setControl(new DutyCycleOut(-0.1));
+    setManipulatorSpeed(-0.1);
   }
 
   private void algaePickup() {
-    m_manipulator.setControl(new DutyCycleOut(1.0));
+    setManipulatorSpeed(1.0);
   }
 
-  public Command stopMovingFactory() {
-    return new RunCommand(() -> stopMoving(), this);
-  }
-
-  public Command moveSlowFactory() {
-    return new RunCommand(() -> moveSlow(), this);
+  public Command stopAllFactory() {
+    return new RunCommand(
+        () -> {
+          funnelStop();
+          manipulatorStop();
+        },
+        this);
   }
 
   public Command algaePickupFactory() {
-    return new RunCommand(() -> algaePickup(), this);
+    return new RunCommand(
+        () -> {
+          funnelStop();
+          algaePickup();
+        },
+        this);
   }
 
   public Command coralIntake() {
     return new RunCommand(
         () -> {
-          moveFast();
-          if (!m_coralRange.getIsDetected().getValue()
-              && !m_doghousCANRange.getIsDetected().getValue()) {
+          if (!hasCoral()) {
             manipulatorIn();
-          } else if (m_coralRange.getIsDetected().getValue()
-              && m_doghousCANRange.getIsDetected().getValue()) {
+            funnelGo();
+          } else if (isBlocked()) {
             manipulatorSlow();
-          } else if (m_coralRange.getIsDetected().getValue()
-              && !m_doghousCANRange.getIsDetected().getValue()) {
+            funnelStop();
+          } else if (!isBlocked()) {
             manipulatorStop();
+            funnelStop();
           }
         },
         this);
   }
 
-  public Command manipulatorOutFactory() {
-    return new RunCommand(() -> manipulatorOut(), this)
+  public Command shootFactory() {
+    return new RunCommand(() -> manipulatorShoot(), this)
         .withTimeout(1.0)
         .andThen(
             () -> {
               manipulatorStop();
-              hasCoral = false;
-              isIn = false;
             });
-  }
-
-  public Command backUpFactory() {
-    return new RunCommand(() -> manipulatorReverse(), this)
-        .until(() -> !m_coralRange.getIsDetected().getValue())
-        .andThen(
-            () -> {
-              manipulatorStop();
-              isIn = true;
-            });
-  }
-
-  public Command manipulatorInFactory() {
-    return new RunCommand(
-        () -> {
-          if (!m_coralRange.getIsDetected().getValue()
-              && !m_doghousCANRange.getIsDetected().getValue()) {
-            manipulatorIn();
-          } else if (m_coralRange.getIsDetected().getValue()
-              && m_doghousCANRange.getIsDetected().getValue()) {
-            manipulatorSlow();
-          } else if (m_coralRange.getIsDetected().getValue()
-              && !m_doghousCANRange.getIsDetected().getValue()) {
-            manipulatorStop();
-          }
-        },
-        this);
-  }
-
-  public Command manipulatorStopFactory() {
-    return new InstantCommand(() -> manipulatorStop(), this);
   }
 
   @Override
   public void periodic() {
     SmartDashboard.putNumber(
         "Doghouse/Funnnel/Current", m_funnel.getStatorCurrent().getValueAsDouble());
-    SmartDashboard.putBoolean(
-        "Doghouse/CoralRange/Is detected", m_coralRange.getIsDetected().getValue());
+    SmartDashboard.putBoolean("Doghouse/Has Coral", hasCoral());
     SmartDashboard.putNumber(
-        "Doghouse/FenceRange/Distance",
+        "Doghouse/Block Distance",
         Units.metersToInches(m_doghousCANRange.getDistance().getValueAsDouble()));
     SmartDashboard.putNumber(
-        "Doghouse/CoralRange/Distance",
+        "Doghouse/Coral Distance",
         Units.metersToInches(m_coralRange.getDistance().getValueAsDouble()));
-    SmartDashboard.putBoolean(
-        "Doghouse/FenceRange/Is detected", m_doghousCANRange.getIsDetected().getValue());
+    SmartDashboard.putBoolean("Doghouse/Is Blocked", isBlocked());
   }
 }
