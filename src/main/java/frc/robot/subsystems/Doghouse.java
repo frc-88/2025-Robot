edca74d0a81dd182b19e8490d635903366b38b2c
Nick Avtges <navtges@gmail.com>
2025-02-22 10:03:15 -0500

    invert doghouse motor, verified prefs

--- src/main/java/frc/robot/subsystems/Doghouse.java
+++ src/main/java/frc/robot/subsystems/Doghouse.java
@@ -8,6 +8,7 @@ import com.ctre.phoenix6.configs.OpenLoopRampsConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -31,6 +32,7 @@ public class Doghouse extends SubsystemBase {
 
   public Doghouse() {
     TalonFXConfiguration doghouseConfiguration = new TalonFXConfiguration();
+    doghouseConfiguration.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
     doghouseConfiguration.CurrentLimits.SupplyCurrentLimit = p_funnelCurrentLimit.getValue();
     doghouseConfiguration.CurrentLimits.SupplyCurrentLimitEnable = true;
     doghouseConfiguration.OpenLoopRamps =
