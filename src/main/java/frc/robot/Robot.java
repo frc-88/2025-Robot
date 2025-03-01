c8c24145f10f8f099462f40e6d00e06d75ea8969
Nick Avtges <navtges@gmail.com>
2025-02-28 21:57:44 -0500

    add command to reset runtime clock

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -18,8 +18,10 @@ import com.ctre.phoenix6.swerve.SwerveModuleConstants.DriveMotorArrangement;
 import com.ctre.phoenix6.swerve.SwerveModuleConstants.SteerMotorArrangement;
 import edu.wpi.first.wpilibj.Threads;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.generated.TunerConstants;
 import frc.robot.util.preferenceconstants.DoublePreferenceConstant;
 
@@ -105,6 +107,7 @@ public class Robot extends LoggedRobot {
     // Instantiate our RobotContainer. This will perform all our button bindings,
     // and put our autonomous chooser on the dashboard.
     robotContainer = new RobotContainer();
+    SmartDashboard.putData("Reset Run Time", new InstantCommand(() -> p_runtime.setValue(0.0)).ignoringDisable(true));
   }
 
   /** This function is called periodically during all modes. */
