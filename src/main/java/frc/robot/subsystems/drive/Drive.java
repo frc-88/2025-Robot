0a05e45bd857e972cb94119f53fa4c5b666711a0
Nick Avtges <navtges@gmail.com>
2025-03-10 09:09:21 -0400

    haiku

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -62,6 +62,10 @@ import java.util.function.BooleanSupplier;
 import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 
+// we stand on shoulders
+// of mechanical giants
+// what an advantage
+
 public class Drive extends SubsystemBase {
   // TunerConstants doesn't include these constants, so they are declared locally
 
